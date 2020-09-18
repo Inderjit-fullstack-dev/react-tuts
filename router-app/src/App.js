d983af53f233b5b5b43c1dadcb837036b1c37cee
inderjit-fullstack-dev <inderjit.fullstack.dev@gmail.com>
2020-09-18 20:11:28 +0530

    routing example done

---
+++ router-app/src/App.js
@@ -0,0 +1,31 @@
+import React, { Component } from "react";
+import NavBar from "./components/navbar";
+import { Route, Switch, Redirect } from "react-router-dom";
+import Products from "./components/products";
+import Posts from "./components/posts";
+import Home from "./components/home";
+import Dashboard from "./components/admin/dashboard";
+import ProductDetails from "./components/productDetails";
+import NotFound from "./components/notFound";
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <NavBar />
+        <Switch>
+          <Route path="/products/:id" component={ProductDetails} />
+          <Route path="/products" component={Products} />
+          <Route path="/posts/:year?/:month?" component={Posts} />
+          <Route path="/admin" component={Dashboard} />
+          <Route path="/" exact component={Home} />
+          {/* <Redirect to="/not-found" /> */}
+          {/* <Redirect from="/message" to="/not-found" /> */}
+        </Switch>
+      </div>
+    );
+  }
+}
+
+export default App;
